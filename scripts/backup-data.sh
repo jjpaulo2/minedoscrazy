697f6d3d81e5f2bd068b6b7a5bb4e91992242e56
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-04 13:52:05 -0300

    feat: new backup script

---
+++ scripts/backup-data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+BACKUP_DIR="backups";
+DATA_DIR="data";
+TIMESTAMP="$(date +%Y%m%d-%H%M%S)";
+BUCKET=$(cat .bucket);
+
+mkdir -p "./$BACKUP_DIR";
+
+echo "[$(date)] Creating backup...";
+
+cp -r "./$DATA_DIR" "./$BACKUP_DIR/data-$TIMESTAMP";
+zip -r "./$BACKUP_DIR/data-$TIMESTAMP.zip" "./$BACKUP_DIR/data-$TIMESTAMP/**";
+
+echo "[$(date)] Uploading backup to S3...";
+
+aws s3 cp "./$BACKUP_DIR/data-$TIMESTAMP.zip" "s3://$BUCKET/data-$TIMESTAMP.zip";
+
+echo "[$(date)] Cleaning backup folder...";
+
+find "$BACKUP_DIR" -mindepth 1 -delete;
+
+echo "[$(date)] Backup completed successfully.";
