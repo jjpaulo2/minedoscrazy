99a982c47f43d30d00301af2f68d61bd52170c07
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-04 14:25:54 -0300

    fix: zip file compressing only the right folder

--- scripts/backup-data.sh
+++ scripts/backup-data.sh
@@ -12,7 +12,9 @@ mkdir -p "./$BACKUP_DIR";
 echo "[$(date)] Creating backup...";
 
 cp -r "./$DATA_DIR" "./$BACKUP_DIR/data-$TIMESTAMP";
-zip -r "./$BACKUP_DIR/data-$TIMESTAMP.zip" "./$BACKUP_DIR/data-$TIMESTAMP";
+cd "./$BACKUP_DIR/data-$TIMESTAMP";
+zip -r "../../data-$TIMESTAMP.zip" ".";
+cd "../..";
 
 echo "[$(date)] Uploading backup to S3...";
 
