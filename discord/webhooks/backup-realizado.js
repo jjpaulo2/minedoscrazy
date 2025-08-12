3de4c7e6818a9caca15b448d4860f12723a5a6d9
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-12 15:03:04 -0300

    fix: fetch api

--- discord/webhooks/backup-realizado.js
+++ discord/webhooks/backup-realizado.js
@@ -4,30 +4,32 @@ const AWS_REGION = process.env.AWS_REGION;
 const BACKUP_FILENAME = process.env.BACKUP_FILENAME;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 
-fetch({
-    method: 'POST',
-    url: DISCORD_WEBHOOK_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "embeds": [
-            {
-                "title": "Backup realizado com sucesso!",
-                "description": "A última versão do mundo foi salva com sucesso. Pode ficar tranquilo!",
-                "color": 5763719,
-                "fields": [
-                    {
-                        "name": "Arquivo",
-                        "value": `[${BACKUP_FILENAME}](https://${S3_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${BACKUP_FILENAME})`
+fetch(
+    DISCORD_WEBHOOK_URL,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "embeds": [
+                {
+                    "title": "Backup realizado com sucesso!",
+                    "description": "A última versão do mundo foi salva com sucesso. Pode ficar tranquilo!",
+                    "color": 5763719,
+                    "fields": [
+                        {
+                            "name": "Arquivo",
+                            "value": `[${BACKUP_FILENAME}](https://${S3_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${BACKUP_FILENAME})`
+                        }
+                    ],
+                    "author": {
+                        "name": GITHUB_USERNAME,
+                        "url": `https://github.com/${GITHUB_USERNAME}`,
+                        "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                     }
-                ],
-                "author": {
-                    "name": GITHUB_USERNAME,
-                    "url": `https://github.com/${GITHUB_USERNAME}`,
-                    "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                 }
-            }
-        ]
-    })
-})
+            ]
+        })
+    }
+)
