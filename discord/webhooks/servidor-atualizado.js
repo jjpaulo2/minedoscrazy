cc357ba2de2b43b74038b06b7611e1f1e573469f
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-12 19:36:02 -0300

    fix: discord notification

--- discord/webhooks/servidor-atualizado.js
+++ discord/webhooks/servidor-atualizado.js
@@ -1,30 +1,32 @@
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 
-fetch({
-    method: 'POST',
-    url: DISCORD_WEBHOOK_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "embeds": [
-            {
-                "title": "Servidor atualizado!",
-                "description": "O servidor acabou de ser atualizado com as últimas configurações do repositório.",
-                "color": 16705372,
-                "fields": [
-                    {
-                        "name": "Repositório",
-                        "value": "[jjpaulo2/minedoscrazy](https://github.com/jjpaulo2/minedoscrazy)"
+fetch(
+    DISCORD_WEBHOOK_URL,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "embeds": [
+                {
+                    "title": "Servidor atualizado!",
+                    "description": "O servidor acabou de ser atualizado com as últimas configurações do repositório.",
+                    "color": 16705372,
+                    "fields": [
+                        {
+                            "name": "Repositório",
+                            "value": "[jjpaulo2/minedoscrazy](https://github.com/jjpaulo2/minedoscrazy)"
+                        }
+                    ],
+                    "author": {
+                        "name": GITHUB_USERNAME,
+                        "url": `https://github.com/${GITHUB_USERNAME}`,
+                        "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                     }
-                ],
-                "author": {
-                    "name": GITHUB_USERNAME,
-                    "url": `https://github.com/${GITHUB_USERNAME}`,
-                    "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                 }
-            }
-        ]
-    })
-})
+            ]
+        })
+    }
+)
--- discord/webhooks/servidor-ligado.js
+++ discord/webhooks/servidor-ligado.js
@@ -2,34 +2,36 @@ const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const DOMAIN_NAME = process.env.DOMAIN_NAME;
 
-fetch({
-    method: 'POST',
-    url: DISCORD_WEBHOOK_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "embeds": [
-            {
-                "title": "Servidor ligado!",
-                "description": `Acesse [${DOMAIN_NAME}/map](https://${DOMAIN_NAME}/map/) para ver o mapa do mundo.`,
-                "color": 5763719,
-                "fields": [
-                    {
-                        "name": "Nome",
-                        "value": "Mine dos Crazy"
-                    },
-                    {
-                        "name": "Endereço",
-                        "value": DOMAIN_NAME
+fetch(
+    DISCORD_WEBHOOK_URL,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "embeds": [
+                {
+                    "title": "Servidor ligado!",
+                    "description": `Acesse [${DOMAIN_NAME}/map](https://${DOMAIN_NAME}/map/) para ver o mapa do mundo.`,
+                    "color": 5763719,
+                    "fields": [
+                        {
+                            "name": "Nome",
+                            "value": "Mine dos Crazy"
+                        },
+                        {
+                            "name": "Endereço",
+                            "value": DOMAIN_NAME
+                        }
+                    ],
+                    "author": {
+                        "name": GITHUB_USERNAME,
+                        "url": `https://github.com/${GITHUB_USERNAME}`,
+                        "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                     }
-                ],
-                "author": {
-                    "name": GITHUB_USERNAME,
-                    "url": `https://github.com/${GITHUB_USERNAME}`,
-                    "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
                 }
-            }
-        ]
-    })
-})
+            ]
+        })
+    }
+)
