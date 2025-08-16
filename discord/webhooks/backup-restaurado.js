d6cb213159d146d7c427cb4fc68824915116b4a5
João Paulo Carvalho <jjpaulo2@protonmail.com>
2025-08-16 15:12:52 -0300

    feat: new action to restore backup

---
+++ discord/webhooks/backup-restaurado.js
@@ -0,0 +1,35 @@
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const S3_BUCKET = process.env.S3_BUCKET;
+const AWS_REGION = process.env.AWS_REGION;
+const BACKUP_FILENAME = process.env.BACKUP_FILENAME;
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+
+fetch(
+    DISCORD_WEBHOOK_URL,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "embeds": [
+                {
+                    "title": "Backup restaurado com sucesso!",
+                    "description": "A merda foi desfeita! Acabamos de restaurar um backup para o mundo.",
+                    "color": 5763719,
+                    "fields": [
+                        {
+                            "name": "Arquivo",
+                            "value": `[${BACKUP_FILENAME}](https://${S3_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${BACKUP_FILENAME})`
+                        }
+                    ],
+                    "author": {
+                        "name": GITHUB_USERNAME,
+                        "url": `https://github.com/${GITHUB_USERNAME}`,
+                        "icon_url": `https://github.com/${GITHUB_USERNAME}.png`
+                    }
+                }
+            ]
+        })
+    }
+)
